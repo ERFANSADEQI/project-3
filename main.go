9d10afea063fbe859095784c32cdaef25bb2bb8b
ERFAN <sadeqierfan00@gmail.com>
2024-05-11 13:12:13 +0330

    Added  publish function

--- main.go
+++ main.go
@@ -1,40 +1,51 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"net/http"
+    "net/http"
 )
 
-var wsupgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-        return true
-    },
+var wsupgrader = websocket.Upgrader {
+	CheckOrigin: func(r *http.Request) bool{
+		return true
+	},
 }
 
 type Subscription struct {
-	Conn *websocket.Conn
-	Topic string
+    Conn  *websocket.Conn
+    Topic string
 }
 
-var subscriptions = make([] *Subscription, 0)
+var subscriptions = make([]*Subscription, 0)
 
 func wsHandler(w http.ResponseWriter, r *http.Request, topic string) {
-	conn , err := wsupgrader.Upgrade(w, r, nil)
-	if err!= nil {
-		http.NotFound(w, r)
+    conn, err := wsupgrader.Upgrade(w, r, nil)
+    if err != nil {
+        http.NotFound(w, r)
         return
     }
-	defer conn.Close()
+    defer conn.Close()
 
-	subscription := &Subscription{Conn: conn, Topic: topic}
-	subscriptions = append(subscriptions, subscription)
+    subscription := &Subscription{Conn: conn, Topic: topic}
+    subscriptions = append(subscriptions, subscription)
 
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err!= nil {
+    for {
+        _, msg, err := conn.ReadMessage()
+        if err != nil {
             break
         }
-		publish(topic, msg)
-	}
+        publish(topic, msg)
+    }
 }
+
+func publish(topic string, msg []byte) {
+    for _, sub := range subscriptions {
+        if sub.Topic == topic {
+            if err := sub.Conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+                return
+            }
+        }
+    }
+}
+
